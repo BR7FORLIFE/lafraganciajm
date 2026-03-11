210c07014566b3851a4fc06ec8f54b1ec25986ef
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-03-11 13:53:42 -0500

    fix: changes some config and upload some assets

--- src/content/content.config.ts
+++ src/content/content.config.ts
@@ -1,1 +1,12 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
+
+const perfumes = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        descripcion: z.string(),
+        price: z.string(),
+    }),
+});
+
+export const collections = { perfumes };
